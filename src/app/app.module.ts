fd98b51ea3e7113913ceda68420e93fd10b0bd0a
Tigran Abrahamyan <tabrahamyan@Tigrans-MacBook-Pro.local>
2020-10-09 11:03:14 +0400

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GameComponent } from './components/game/game.component';
+import { ResultComponent } from './components/result/result.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    GameComponent,
+    ResultComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+
+export class AppModule { }
