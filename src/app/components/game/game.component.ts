d8f6c4b67c17cad3eb25b176a8b9cde6e2c47fe7
TAbrahamyan <t.abrahamyann@gmail.com>
2020-10-10 18:13:35 +0400

    Implementing game refreshing

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -8,17 +8,23 @@ import { Component } from '@angular/core';
 
 export class GameComponent {
   timer: number = 60;
+  interval: ReturnType<typeof setInterval>;
 
   get formatTimer(): string {
     return new Date(this.timer * 1000).toISOString().substr(14, 5);
   }
 
+  resetGameHandler(): void {
+    this.timer = 60;
+    clearInterval(this.interval);
+  }
+
   startTypingHandler(): void {
-    const interval = setInterval(() => {
+    this.interval = setInterval(() => {
       this.timer--;
 
       if (this.timer <= 0) {
-        clearInterval(interval);
+        clearInterval(this.interval);
       }
     }, 1000);
   }
